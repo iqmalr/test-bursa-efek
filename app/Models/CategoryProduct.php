717fc69c00aa759b06e774bdd7135891fa6a5a95
Iqmal Riffai <m.iqmal.riffai@gmail.com>
2025-03-25 21:34:40 +0700

    setup Model CategoryProduct and Product

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryProduct extends Model
 {
     use HasFactory;
+
+    protected $table = 'category_products';
+    protected $fillable = ['name'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'product_category_id');
+    }
 }
